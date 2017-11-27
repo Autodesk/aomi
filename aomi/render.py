40f58f209e10afda9bf50a700f324f34eca1ad1d
Eric Fode <ericfode@gmail.com>
2017-11-27 13:33:02 -0800

    Update render.py

--- aomi/render.py
+++ aomi/render.py
@@ -165,7 +165,7 @@ def env(client, paths, opt):
                                      not opt.merge_path)
     if old_prefix:
         LOG.warning("the prefix option is deprecated "
-                    "please use" 
+                    "please use"
                     "--no-merge-path --add-prefix $OLDPREFIX_ instead")
     elif opt.prefix:
         LOG.warning("the prefix option is deprecated"
